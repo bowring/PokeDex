76282f2ccd3a07b78a50b5bde4afa0f17352dd49
JalenEvans <jalenemmanuelevans@gmail.com>
2023-03-21 14:52:18 -0400

    Working with my written code in replace of the example code. The only exceptions being the Pokemon folder is not in the project yet and the stylesheet has not been addedd.

--- PokeDex/PokeDexApp/src/main/java/com/example/pokedexapp/PokeDexAppController.java
+++ PokeDex/PokeDexApp/src/main/java/com/example/pokedexapp/PokeDexAppController.java
@@ -1,14 +1,72 @@
 package com.example.pokedexapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.HashMap;
 
 public class PokeDexAppController {
+    private Stage stage;
     @FXML
-    private Label welcomeText;
+    private TextField nameField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private String name;
+
+    @FXML
+    private Label testLabel;
+
+    @FXML
+    private Button showBtn;
+
+    @FXML
+    private Button pickBtn;
+
+    @FXML
+    private ImageView pokemon;
+
+    @FXML
+    private Image pokeImage;
+
+    private HashMap<String, String> pokeNames = new HashMap<>();
+
+    public PokeDexAppController() {
+        pokeNames.put("bulbasaur", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\bulbasaur.png");
+        pokeNames.put("charmander", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\charmander.png");
+        pokeNames.put("squirtle", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\squirtle.png");
+        pokeNames.put("pikachu", "C:\\Users\\redfl\\Desktop\\CIRDLES\\Pokemon\\pikachu.png");
+
+        stage = new Stage();
     }
+
+    @FXML
+    private void showPokemon() {
+        name = nameField.getText();
+        pokeImage = new Image(pokeNames.get(name));
+        pokemon.setImage(pokeImage);
+        testLabel.setText(name);
+    }
+    @FXML
+    private void pickPokemon() {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            testLabel.setText(removeExtension(file.getName()));
+            pokeImage = new Image(file.getAbsolutePath());
+            pokemon.setImage(pokeImage);
+        }
+        else {
+            testLabel.setText("Nothing new was selected");
+        }
+    }
+
+    private String removeExtension (String filename) { return filename.split("\\.")[0]; }
 }
