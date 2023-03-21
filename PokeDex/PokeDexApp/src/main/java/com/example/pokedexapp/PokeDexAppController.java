70a4213a48dd0d0ff114073ca69eae4ad0fb8515
JalenEvans <jalenemmanuelevans@gmail.com>
2023-03-21 14:46:49 -0400

    Working with renamed files

--- PokeDex/PokeDexApp/src/main/java/com/example/pokedexapp/PokeDexAppController.java
+++ PokeDex/PokeDexApp/src/main/java/com/example/pokedexapp/PokeDexAppController.java
@@ -3,7 +3,7 @@ package com.example.pokedexapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class PokeDexAppController {
     @FXML
     private Label welcomeText;
 
