81118c80022ac22c25b66049dda4e3ad61b515d5
JalenEvans <jalenemmanuelevans@gmail.com>
2023-03-21 14:39:41 -0400

    Working version of JavaFX example code with App and Core modules

---
+++ PokeDex/PokeDexApp/src/main/java/com/example/pokedexapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.pokedexapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
